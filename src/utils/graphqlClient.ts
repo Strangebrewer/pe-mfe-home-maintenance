f8b0e533067e8c0ea377fb151f7df28fcf4e4b76
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-07-28 01:59:22 -0600

    refactoring while Rome burns

--- src/utils/graphqlClient.ts
+++ src/utils/graphqlClient.ts
@@ -1,17 +1,6 @@
 import { axiosAuth } from './authClient';
+import { baseGqlRequest } from '@bka-stuff/pe-mfe-utils';
 
 const GQL_URL = process.env.GQL_URL || 'http://localhost:4000';
 
-export async function gqlRequest<T = any>(
-  query: string,
-  variables?: Record<string, any>,
-  traceId?: string,
-): Promise<T> {
-  const headers: Record<string, any> = {};
-  if (traceId) headers['X-Trace-ID'] = traceId;
-  const response = await axiosAuth.post(GQL_URL, { query, variables }, { headers });
-  if (response.data.errors) {
-    throw new Error(response.data.errors[0].message);
-  }
-  return response.data.data;
-}
+export const gqlRequest = baseGqlRequest(GQL_URL, axiosAuth);
